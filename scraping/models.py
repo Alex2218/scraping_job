b048e689469e8e4a3b4e46e72fcc26a29a684bae
Alex2218 <alexkravchenko116@gmail.com>
2021-12-01 12:36:23 +0200

    Improvements in the display of site pages

---
+++ scraping/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from slugify import slugify
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Город', unique=True)  # verbose_name - удобночитаемое имя для поля, используемого в поле метки
+    slug = models.SlugField(max_length=50, blank=True, unique=True)  # unique - чтобы название города было уникальным
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+
+    def __str__(self):
+        return self.name
+
+    def save(self,  *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(str(self.name))
+        super().save(*args, **kwargs)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Язык программирования', unique=True)
+    slug = models.SlugField(max_length=50, blank=True, unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)  # unique - чтобы ссылка была уникальной
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE, verbose_name='Язык программирования')
+    timestamp = models.DateField(auto_now_add=True)  # день когда внесли ваканию, auto_now_add - автоматичкое проставление даты после попадение записи в БД
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
