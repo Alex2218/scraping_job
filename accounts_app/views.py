d0de7aea913a5862c05fab6badd643af8b90a8fa
Alex2218 <alexkravchenko116@gmail.com>
2021-12-09 16:38:07 +0200

    Created an account for a new user

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout, get_user_model
-from accounts_app.forms import UserLoginForm
+from accounts_app.forms import UserLoginForm, UserRegistrationForm,UserUpdateForm
+
+
+User = get_user_model()
 
 def login_view(request):
     form = UserLoginForm(request.POST or None)
@@ -17,4 +19,43 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    return redirect('home')
+
+
+def register_view(request):
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'accounts_app/register_done.html',
+                      {'new_user': new_user})
+    return render(request, 'accounts_app/register.html', {'form': form})
+
+
+def update_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = UserUpdateForm(request.POST)
+            if form.is_valid():
+                data = form.cleaned_data
+                user.city = data['city']
+                user.language = data['language']
+                user.send_email = data['mailing']
+                user.save()
+                return redirect('accounts:update')
+        form = UserUpdateForm(initial={'city': user.city, 
+                'language': user.language,'mailing': user.mailing})
+        return render(request, 'accounts_app/update.html',{'form': form,})
+    else:
+        return redirect('accounts:login')
+
+
+def delete_view(request):
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            qs = User.objects.get(pk=user.pk)
+            qs.delete()
     return redirect('home')
