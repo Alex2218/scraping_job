68a548ac848b2440cd94402cc2d163dc3bc242cf
Alex2218 <alexkravchenko116@gmail.com>
2021-12-18 16:28:14 +0200

    Added three classes

--- job_ua/urls.py
+++ job_ua/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from scraping.views import home_view, list_view
+from scraping.views import VacancyDelete, VacancyUpdate, home_view, list_view, \
+    VacancyDetail, VacancyCreate
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('list/', list_view, name='list'),
     path('accounts_app/', include(('accounts_app.urls', 'accounts'))),
+    path('detail/<int:pk>/', VacancyDetail.as_view(), name='detail'),
+    path('create/', VacancyCreate.as_view(), name='create'),
+    path('update/<int:pk>/', VacancyUpdate.as_view(), name='update'),
+    path('delete/<int:pk>/', VacancyDelete.as_view(), name='delete'),
     path('', home_view, name='home'),
 
 ]
--- scraping/views.py
+++ scraping/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.views.generic.edit import DeleteView, UpdateView
 from .models import Vacancy
-from .forms import FindForm
+from .forms import FindForm, VacancyForm
+from django.urls import reverse_lazy
 from django.core.paginator import Paginator
+from django.views.generic import DetailView, CreateView
 
 
 def home_view(request):
@@ -21,9 +25,37 @@ def list_view(request):
         if language:
             _filter['language__name'] = language
 
-        qs = Vacancy.objects.filter(**_filter)
+        qs = Vacancy.objects.filter(**_filter).select_related('city', 'language')
         paginator = Paginator(qs, 10)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         context['object_list'] = page_obj
     return render(request, 'scraping/list.html', context)
+
+
+class VacancyDetail(DetailView):
+    queryset = Vacancy.objects.all()
+    template_name = 'scraping/detail.html'
+
+
+class VacancyCreate(CreateView):
+    model = Vacancy
+    form_class = VacancyForm
+    template_name = 'scraping/create.html'
+    success_url = reverse_lazy('home')
+
+
+class VacancyUpdate(UpdateView):
+    model = Vacancy
+    form_class = VacancyForm
+    template_name = 'scraping/create.html'
+    success_url = reverse_lazy('home')
+
+
+class VacancyDelete(DeleteView):
+    model = Vacancy
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        messages.success(request, 'Запись успешно удалена.')
+        return self.post(request, *args, **kwargs)
